76ba6228d1158ae1cb5f48c222cfb255a1067244
cao2 <cao2@mail.usf.edu>
2015-09-22 21:36:45 -0400

    ll

--- src/node.java
+++ src/node.java
@@ -1,9 +1,16 @@
 public class Node { 
+	
     private String name; 
     private Map<Node,Integer> child=new HashMap<Node,Integer>(); 
+    
     public Node(String name){ 
         this.name=name; 
     } 
+    public Node(){
+    	this.name=non;
+    	this.child=non;
+    	
+    }
     public String getName() { 
         return name; 
     } 
@@ -13,6 +20,7 @@ public class Node {
     public Map<Node, Integer> getChild() { 
         return child; 
     } 
+    
     public void setChild(Map<Node, Integer> child) { 
         this.child = child; 
     } 
